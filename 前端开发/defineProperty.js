cebd3b4769330b992c444b97036cae56f5789dd6
陈双龙 <1390158928@qq.com>
2021-02-19 21:57:55 +0800

    monkey patch(猴子补丁),不安全也不是很优雅

--- 前端开发/defineProperty.js
+++ 前端开发/defineProperty.js
@@ -7,19 +7,58 @@ let data = {
   }
 }
 
-Object.keys(data).forEach(key => {
-  let currentValue = data[key]
+// Object.keys(data).forEach(key => {
+//   let currentValue = data[key]
 
-  Object.defineProperty(data,key,{
-    enumerable: true,
-    configurable: false,
-    get() {
-      console.log(`getting ${key} value now,getting value is:`,currentValue);
-      return currentValue;
-    },
-    set(newValue) {
-      currentValue = newValue;
-      console.log(`setting ${key} value now, setting value is:`,currentValue);
-    }
+//   Object.defineProperty(data,key,{
+//     enumerable: true,
+//     configurable: false,
+//     get() {
+//       console.log(`getting ${key} value now,getting value is:`,currentValue);
+//       return currentValue;
+//     },
+//     set(newValue) {
+//       currentValue = newValue;
+//       console.log(`setting ${key} value now, setting value is:`,currentValue);
+//     }
+//   })
+// })
+
+// data.stage
+// console.log(data.stage);
+// data.stage.title = 'test';
+// console.log(data.stage);
+const observe = data => {
+  if(!data || typeof data !== 'object'){
+    return ;
+  }
+
+  Object.keys(data).forEach(key => {
+    let currentValue = data[key];
+
+    observe(currentValue);
+
+    Object.defineProperty(data,key,{
+      configurable:true,
+      enumerable: true,
+      get() {
+        console.log(`getting ${key} value now,getting value is:`,currentValue);
+        return currentValue;
+      },
+      set(newValue) {
+        currentValue = newValue;
+        if( typeof newValue == 'object'){
+          observe(currentValue) //当新赋值类型为对象时，为新赋值对象的值的属性添加拦截
+        }
+        console.log(`setting ${key} value now, setting value is`,currentValue);
+      }
+
+    })
   })
-})
+}
+observe(data)
+// data.stage= 'test';
+data.course.author= {t:'t'};
+data.course.author.t = 'test'
+// console.log(data.stage);
+
