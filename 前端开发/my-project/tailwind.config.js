2ffe2f9725bc789748c3ff254f42210bd81d2dad
陈双龙 <1390158928@qq.com>
2021-03-18 19:03:04 +0800

    tailwind

--- 前端开发/my-project/tailwind.config.js
+++ 前端开发/my-project/tailwind.config.js
@@ -1,11 +1,31 @@
+const { plugin } = require("postcss");
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['active','group-focus'],
+      scale: ['focus-within'],
+      opacity:['disabled']
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addUtilities }){
+      const newUtilities = {
+        '.filter-none': {
+          filter: 'none',
+        },
+        '.filter-grayscale': {
+          filter: 'grayscale(100%)',
+        }
+      }
+      addUtilities(newUtilities,['responsive','hover'])
+    })
+  ],
+
+
 }
