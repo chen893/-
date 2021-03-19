f3642b963ac4ccd1f72adee5597a384affa46e01
陈双龙 <1390158928@qq.com>
2021-03-19 12:42:10 +0800

    aa

--- 前端开发/my-project/tailwind.config.js
+++ 前端开发/my-project/tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    }
   },
   variants: {
     extend: {
