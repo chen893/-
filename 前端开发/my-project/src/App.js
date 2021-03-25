a2fb54e173e8775e3988b6491488871987526e8d
陈双龙 <1390158928@qq.com>
2021-03-25 22:35:04 +0800

    all

--- 前端开发/my-project/src/App.js
+++ 前端开发/my-project/src/App.js
@@ -3,18 +3,58 @@ import './App.css';
 
 function App() {
   return (
-    // <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-    //   <div class="md:flex">
-    //     <div class="md:flex-shrink-0">
-    //       <img class="h-48 w-full object-cover md:w-48" src="/img/store.jpg" alt="Man looking at item at a store" />
-    //     </div>
-    //     <div class="p-8">
-    //       <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
-    //       <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
-    //       <p class="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
-    //     </div>
-    //   </div>
-    // </div>
+<div>
+  <div class="md:grid md:grid-cols-3 md:gap-6  bg-gray-500" >
+    <div class="md:col-span-1">
+      <div class="px-4 sm:px-0">
+        <h3 class="text-lg font-medium leading-6 text-gray-900">Profile</h3>
+        <p class="mt-1 text-sm text-gray-600">
+          This information will be displayed publicly so be careful what you share.
+        </p>
+      </div>
+    </div>
+    <div class="mt-5 md:mt-0 md:col-span-2">
+      <form action="#" method="POST">
+        <div class="shadow sm:rounded-md sm:overflow-hidden">
+          <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+            <div class="grid grid-cols-3 gap-6">
+              <div class="col-span-3 sm:col-span-2">
+                <label for="company_website" class="block text-sm font-medium text-gray-700">
+                  用户名
+                </label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                  <input type="text" name="company_website" id="company_website" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="www.example.com" />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <label for="about" class="block text-sm font-medium text-gray-700">
+                内容
+              </label>
+              <div class="mt-1">
+                <textarea id="about" name="about" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="you@example.com"></textarea>
+              </div>
+              <p class="mt-2 text-sm text-gray-500">
+                Brief description for your profile. URLs are hyperlinked.
+              </p>
+            </div>
+
+
+
+          </div>
+          <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+            <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Save
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+/*
     <div>
 <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto ">
@@ -27,23 +67,16 @@ function App() {
         <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">1.8K</h2>
         <p class="leading-relaxed">留言数</p>
       </div>
-      {/* <div class="p-4 sm:w-1/4 w-1/2">
-        <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">35</h2>
-        <p class="leading-relaxed">Downloads</p>
-      </div>
-      <div class="p-4 sm:w-1/4 w-1/2">
-        <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">4</h2>
-        <p class="leading-relaxed">Products</p>
-      </div> */}
     </div>
-    <div class="bg">
-
+    <div class='flex flex-col'>
+      <input type='text' placeholder='留言内容' class='focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10'></input>
+      <input type='text' placeholder='用户名' class='focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10 mt-3' ></input>
     </div>
-
+      <button class='text-base font-medium rounded-lg p-3 bg-gray-500 text-white'>提交</button>
   </div>
 </section>
 
-    </div>
+    </div> */
   );
 }
 
